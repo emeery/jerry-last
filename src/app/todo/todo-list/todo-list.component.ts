efc836bbefa43d662da8bd91b2f2bf939335a5ca
emeery <gerardoemery@live.com.mx>
2026-01-04 18:38:20 -0600

    fix firestore

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -12,6 +12,7 @@ export class TodoListComponent implements OnInit{
   ) {}
 
   ngOnInit(): void {
-    this.todoService.fetchTodo();
+    this.todoService.fetchTodo()
+    
   }
 }
--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { map } from 'rxjs';
 import { Todo } from './todo.model';
 import { collection } from 'firebase/firestore';
@@ -8,24 +8,25 @@ import { collection } from 'firebase/firestore';
   providedIn: 'root'
 })
 export class TodoService {
-  // userCollection!: AngularFirestoreCollection;
-  collection: any;
-  constructor(  
+  todoCollection!: AngularFirestoreCollection;
+  constructor(
     private db: AngularFirestore
   ) { }
 
-//    fetchAvaliableExercises
-fetchTodo() {
-      this.db.collection('todo').valueChanges().subscribe(todo => {
-      console.log('t',todo);  
-    })
-        
-    }
+  //    fetchAvaliableExercises
+  fetchTodo() {
+    this.todoCollection = this.db.collection<Todo>('todo');
+    this.todoCollection.valueChanges({ idField: 'id' })
+      .subscribe(todos => {
+        console.log(todos);
+      })
 
-   /*  getItems(): Observable<Todo[]> {
-    const itemsCollection = collection(this.firestore, 'todo');
-    // collectionData streams the documents as an array of JSON objects
-    return collectionData(itemsCollection, { idField: 'id' }) as Observable<Todo[]>;
-  } */
-    
+  }
+
+  /*  getItems(): Observable<Todo[]> {
+   const itemsCollection = collection(this.firestore, 'todo');
+   // collectionData streams the documents as an array of JSON objects
+   return collectionData(itemsCollection, { idField: 'id' }) as Observable<Todo[]>;
+ } */
+
 }
